e63473e6b8cbe227357adc58e0321b74a5a40acc
romero126 <romero126@gmail.com>
2018-06-26 10:00:58 -0700

    Bug Fixes, Code Cleanup
    Fixed a ton of bugs. Thanks @ChrisDent

--- PSCallTrace/Public/New-PSCallTraceHook.ps1
+++ PSCallTrace/Public/New-PSCallTraceHook.ps1
@@ -22,50 +22,21 @@ function New-PSCallTraceHook {
         [Parameter()]
         [Switch]$DisableParamMirroring
     )
-    $HookFunction = {
-        [CmdletBinding()]
-        param()
-        !DynamicParam
-        begin {
-            $PSStackData = Invoke-PSCallTrace -Begin
-            $wrappedCmd = (get-command !Module\!Name)
-            $scriptCmd = {& $wrappedCmd @PSBoundParameters }
-            $steppablePipeline = $scriptCmd.GetSteppablePipeline()
-            $steppablePipeline.Begin($PSCmdlet)
-        }
-        process {
-            Invoke-PSCallTrace -Object $PSStackData -Process
-            $steppablePipeline.Process($_)
-        }
-        end {
-            Invoke-PSCallTrace -Object $PSStackData -End
-            $steppablePipeline.End()
-        }
-    }
     if ($Module -eq "PSCallTraceHook") {
         if (Test-Path ("Function:{0}\{1}" -f $Module, $Name)) {
             Throw "Cannot Hook Function, It looks like it is already hooked"
         }
         Rename-Item -Path "Function:Global:$Name" -NewName ("Global:{0}\{1}" -f $Module, $Name)
     }
-    
-    $DynamicParam = ""
-    if (!$DisableParamMirroring) {
-        $DynamicParam = {
-            dynamicparam {
-                Get-BaseParameter "!Module\!Name"
-            }
-        }.ToString()
-    }
-    $HookFunction = $HookFunction.ToString()
-    $HookFunction = $HookFunction -Replace "!DynamicParam", $DynamicParam
-    $HookFunction = $HookFunction -Replace "!Module", $Module -Replace "!Name", $Name
-    $HookFunction = [ScriptBlock]::Create(
-        $HookFunction
-    )
+    $MetaData = New-Object System.Management.Automation.CommandMetaData (Get-Command "$Module\$Name")
+    $HookedFunction = [System.Management.Automation.ProxyCommand]::Create($MetaData)
+    $HookedFunction = $HookedFunction -Replace "steppablePipeline.Begin", "PSStackData = Invoke-PSCallTrace -Begin; `$steppablePipeline.Begin"
+    $HookedFunction = $HookedFunction -Replace "steppablePipeline.Process", "PSStackData = Invoke-PSCallTrace -Process; `$steppablePipeline.Process"
+    $HookedFunction = $HookedFunction -Replace "steppablePipeline.End", "PSStackData = Invoke-PSCallTrace -End; `$steppablePipeline.End"
+
     if (Test-Path Function:$Name) {
-        Set-Item -Path Function:Global:$Name -Value $HookFunction | out-null
+        Set-Item -Path Function:Global:$Name -Value $HookedFunction | out-null
     } else {
-        New-Item -Path Function: -Name "Global:$Name" -Value $HookFunction | out-null
+        New-Item -Path Function: -Name "Global:$Name" -Value $HookedFunction | out-null
     }
 }
--- Tests/FunctionHook.Test.ps1
+++ Tests/FunctionHook.Test.ps1
@@ -1,10 +1,11 @@
 Import-Module .\PSCallTrace\PSCallTrace.psd1  -Global | out-null
 
 function Test-PSHook {
-    Write-Host "I was called"
+    Write-Host "I was called", $args
 }
 New-PSCallTraceHook -Name "Test-PSHook"
 #New-PSCallTraceHook -Name "Test-PSHook"
+
 Test-PSHook
-Test-PSHook 
+Test-PSHook "asdf"
 Get-PSCallTrace | ft -a
